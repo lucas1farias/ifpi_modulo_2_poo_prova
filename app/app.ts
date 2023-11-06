83fbf0307be16f6aa4ea6d42972a752b8949cb50
lucas1farias <fariasluuk@gmail.com>
2023-11-06 12:04:53 -0300

    new updates

--- app/app.ts
+++ app/app.ts
@@ -12,25 +12,7 @@ function main() {
     const repProfiles: RepositorioDePerfis = new RepositorioDePerfis(profiles)
     const repPosts: RepositorioDePostagens = new RepositorioDePostagens(posts)
     const socialMedia: RedeSocial = new RedeSocial(repProfiles, repPosts)
-    
-    const exe = prompt()
-    console.log("Qual algoritmo deve ser executado?\n1. Principal\n2. Testes")
-    let option = exe(">>> ")
-    switch(option) {
-        case "0":
-            return
-        case "1":
-            new App(socialMedia, true)
-            break
-        case "2":
-            new Algorithm(true) 
-            break
-        default:
-            console.log("Opção inválida!")
-    }
-    // option === "1" ? new App(socialMedia, true) : new Algorithm(true)  
-    // const testAlgorithm: Algorithm = new Algorithm()
-    // const app: App = new App(socialMedia, true)
+    const app = new App(socialMedia, true)
 }
 
 main()
